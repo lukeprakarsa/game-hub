44b25e55af6603f654faa4c82d7bf255f0e7e7b6
Luke Prakarsa <luke.prakarsa@gmail.com>
2024-01-07 16:24:52 -0800

    Show genre skeletons

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,34 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6];
+
+  if (error) return null;
+
+  if (isLoading)
+    return (
+      <List>
+        {skeletons.map((s) => (
+          <ListItem key={s} paddingY="5px">
+            <HStack>
+              <Skeleton boxSize="32px" borderRadius={8} />
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    );
+
   return (
     <List>
       {data.map((g) => (
