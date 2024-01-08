cc497d1be78c14fde7246f148469f3e6227eba2f
Luke Prakarsa <luke.prakarsa@gmail.com>
2024-01-07 18:28:53 -0800

    Highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,17 +5,16 @@ import {
   List,
   ListItem,
   Skeleton,
-  SkeletonText,
-  Text,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
@@ -45,6 +44,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               borderRadius={8}
             />
             <Button
+              fontWeight={g.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(g)}
               fontSize="lg"
               variant="link"
